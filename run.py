a8ceaa67a42318a6e880d7c268e33aeabb0151f8
Mr. Apache <abrahampacheco2885@gmail.com>
2026-03-31 23:03:40 -0400

    Initialize database and configure app run settings
    Added database initialization and updated app run configuration.

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
-from app import create_app
+from app import create_app, db
+import os
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# ESTE ES EL BLOQUE MÁGICO PARA EL PLAN GRATUITO
+with app.app_context():
+    # Esto crea las 11 tablas automáticamente al arrancar
+    db.create_all()
+    print("Base de datos calibrada y lista.")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
