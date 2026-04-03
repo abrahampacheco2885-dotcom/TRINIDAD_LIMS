6d797bc0358b3064c62c4d2fdba0e98e14486615
Mr. Apache <abrahampacheco2885@gmail.com>
2026-04-03 11:07:07 -0400

    Update run.py to use dynamic port from environment

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
+import os
 from app import create_app, db
 
 app = create_app()
 
 if __name__ == '__main__':
+    # Render asigna un puerto en la variable 'PORT'
+    port = int(os.environ.get("PORT", 5000))
+    
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000)
+        
+    # Importante: host='0.0.0.0' es obligatorio para Render
+    app.run(host='0.0.0.0', port=port)
