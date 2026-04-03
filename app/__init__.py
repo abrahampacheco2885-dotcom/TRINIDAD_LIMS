fd966ea21a7054ad48fb13e1c76a4da3802e3752
Mr. Apache <abrahampacheco2885@gmail.com>
2026-04-03 11:43:58 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 @app.route('/')
 @login_required
 def index():
+    from flask import render_template
+    # Esto te enviará al menú de los 3 botones (Pacientes, Caja, Resultados)
     return render_template('index.html')
